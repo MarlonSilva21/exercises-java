c504337ea2a8fdadd1ea1e912cac69dfb7873152
MarlonSilva21 <josemarlondasilva48@gmail.com>
2021-12-01 19:42:09 -0300

    Default methods

---
+++ src/model/services/UsaInterestService.java
@@ -0,0 +1,16 @@
+package model.services;
+
+public class UsaInterestService implements InterestService{
+
+    private Double interestRate;
+
+    public UsaInterestService(Double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    @Override
+    public Double getInterestRate() {
+        return interestRate;
+    }
+
+}
