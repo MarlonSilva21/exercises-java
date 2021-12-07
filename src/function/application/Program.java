9965c6ff3823b8d57dfcd4498956b02669e0e716
MarlonSilva21 <josemarlondasilva48@gmail.com>
2021-12-07 16:21:40 -0300

    Function - Map

---
+++ src/function/application/Program.java
@@ -0,0 +1,25 @@
+package function.application;
+
+import consumer.entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 500.00));
+        list.add(new Product("HD Case", 80.00));
+
+        List<String> namesWithUpperCase = list.stream().map(product -> product.getName().toUpperCase()).collect(Collectors.toList());
+
+        namesWithUpperCase.forEach(System.out::println);
+    }
+}
