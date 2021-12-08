7880e02e79f59340340c7203e7fe77c20a7f3ef6
MarlonSilva21 <josemarlondasilva48@gmail.com>
2021-12-08 13:45:23 -0300

    Optional

---
+++ src/optional/example.java
@@ -0,0 +1,35 @@
+package optional;
+
+import java.util.Optional;
+
+public class example {
+
+    public static void main(String[] args) {
+
+        String s = "1";
+        converteEmNumero(s).ifPresent(System.out::println); // ifPresent -> verifica se tem um valor presente com um if implícito e executa a lambda
+
+        Integer numero = converteEmNumero(s).orElse(5); //orElse -> funciona como um valor default caso nao tenha um valor definiddo
+        System.out.println(numero);
+
+        Integer numero1 = converteEmNumero(s).orElseGet(() -> 5); //orElseGet -> a diferença para o orElse é que ele recebe uma expressão lambda e pode fazer uma operação mais pesada
+        System.out.println(numero1);
+
+        Integer numero2 = converteEmNumero(s).orElseThrow(() -> new NullPointerException("Valor vazio")); //orElseThrow lança uma excessão
+        System.out.println(numero2);
+
+
+
+    }
+
+    public static Optional<Integer> converteEmNumero(String numeroStr){
+
+        try{
+            Integer integer = Integer.valueOf(numeroStr);
+            return Optional.of(integer);
+        }
+        catch (Exception e){
+            return Optional.empty();
+        }
+    }
+}
