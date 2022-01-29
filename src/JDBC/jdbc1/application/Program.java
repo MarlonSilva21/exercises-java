0dac47c460c6621b25a53b96b9e44c8830cf14fb
MarlonSilva21 <josemarlondasilva48@gmail.com>
2022-01-29 18:16:05 -0300

    Exercise JDBC 1 - Connecting DB

---
+++ src/JDBC/jdbc1/application/Program.java
@@ -0,0 +1,18 @@
+package JDBC.jdbc1.application;
+
+import JDBC.jdbc1.db.DB;
+
+import java.sql.Connection;
+
+
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Connection conn =  DB.getConnection();
+		DB.closeConnection();
+
+	}
+
+}
